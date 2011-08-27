ad32c6bd390490668b049134463baf88577e9a35
Matthew Morrison <matthewmorrison@matthew-morrisons-macbook.local>
2011-08-27 15:22:15 -0500

    moved features

--- mattspackage/tests/__init__.py
+++ mattspackage/tests/__init__.py
@@ -1,3 +1,8 @@
 from pkg_resources import load_entry_point
 
+from os import path
+import sys
+
+sys.argv[1] = path.abspath(path.join(path.dirname(path.abspath(__file__)), 'features'))
 load_entry_point('lettuce', 'console_scripts', 'lettuce')()
+
--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import find_packages, setup
 
 setup(
-    version="0.0.5",
+    version="0.1.5",
     name="mattspackage",
     author="Matthew J. Morrison",
     author_email="mattjmorrison@mattjmorrison.com",
     description="Matt's Package will notify werewolf hunters when the next full moon will be.",
     long_description=open('README', 'r').read(),
     packages=find_packages(),
+    include_package_data=True,
+    classifiers = [
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Environment :: Console',
+    ],
     install_requires=(
         'pyephem',
     ),
